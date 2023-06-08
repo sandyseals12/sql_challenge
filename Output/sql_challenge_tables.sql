9526a75ec2e9d009b3acaa1eeb74e37c85a824c0
sandyseals12 <129459498+sandyseals12@users.noreply.github.com>
2023-06-08 17:20:08 -0400

    Update sql_challenge_tables.sql

--- Output/sql_challenge_tables.sql
+++ Output/sql_challenge_tables.sql
@@ -1,15 +1,18 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
 CREATE TABLE "Department" (
-    "dept_no" VARCHAR NOT NULL,
-    "dept_name" VARCHAR NOT NULL,
-    CONSTRAINT "pk_Department" PRIMARY KEY ("dept_no")
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL
 );
 
 CREATE TABLE "Dept_emp" (
-    "emp_ct" SERIAL NOT NULL,
-    "emp_no" INT NOT NULL,
-    "dept_no" VARCHAR NOT NULL,
-    CONSTRAINT "pk_Dept_emp" PRIMARY KEY ("emp_ct"),
-    CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY ("dept_no") REFERENCES "Department" ("dept_no")
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Dept_emp" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 
@@ -20,25 +23,33 @@ CREATE TABLE "Dept_manager" (
 );
 
 CREATE TABLE "Employees" (
-    "emp_no" SERIAL NOT NULL,
-    "emp_title_ID" VARCHAR NOT NULL,
-    "birth_date" VARCHAR NOT NULL,
-    "first_name" VARCHAR NOT NULL,
-    "last_name" VARCHAR NOT NULL,
-    "sex" VARCHAR NOT NULL,
-    "hire_date" VARCHAR NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY ("emp_no")
+    "emp_no" INT   NOT NULL,
+    "emp_title_ID" Varchar   NOT NULL,
+    "birth_date" Date   NOT NULL,
+    "first_name" Varchar   NOT NULL,
+    "last_name" Varchar   NOT NULL,
+    "sex" Varchar   NOT NULL,
+    "hire_date" Date   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "Salaries" (
-    "emp_no" SERIAL NOT NULL,
-    "salary" INT NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY ("emp_no"),
-    CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY ("emp_no") REFERENCES "Employees" ("emp_no")
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "Titles" (
-    "title_Id" VARCHAR NOT NULL,
-    "title" VARCHAR NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY ("title_Id")
+    "title_Id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_Id"
+     )
 );
+
+
+
